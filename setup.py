e68e88b1c4a5ca41c3692699db9cff701cde8374
trankov <trankov@ya.ru>
2025-04-07 06:10:22 +0500

    fix windows encoding issue

--- setup.py
+++ setup.py
@@ -11,7 +11,9 @@ setup(
     description='Fast UTF-8-safe HTML to text converter',
     author='Aleksei Trankov',
     author_email='shoyna@ognzm.ru',
-    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description=(Path(__file__).parent / 'README.md').read_text(
+        encoding='utf_8_sig'
+    ),
     long_description_content_type='text/markdown',
     ext_modules=[module],
     python_requires='>=3.8',
