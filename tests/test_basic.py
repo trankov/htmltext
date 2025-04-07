edaa1c47a56f57803c40c32dd9cee61d099cafde
trankov <trankov@ya.ru>
2025-04-07 05:58:24 +0500

    fix test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,12 @@
 import htmltext
 
+
 def test_text_from_html():
     html = "<div>Привет <b>мир</b>!</div><p>Тест</p>"
     result = htmltext.text_from_html(html)
-    assert result == "Привет мир!\nТест"
+    assert result == '\nПривет мир!\nТест\n'
+
+
+if __name__ == "__main__":
+    test_text_from_html()
+    print("Test passed.")
